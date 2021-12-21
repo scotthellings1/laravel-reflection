5eb5012cbc8521f77feaab58c4c8c79007f561f4
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-12-21 12:45:05 +0000

    generated employee,company -> model,controller,form request,migration,seeder,factory

--- routes/auth.php
+++ routes/auth.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/register', [RegisteredUserController::class, 'create'])
-                ->middleware('guest')
-                ->name('register');
-
-Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest');
+// removed routes to disable registration
+//Route::get('/register', [RegisteredUserController::class, 'create'])
+//                ->middleware('guest')
+//                ->name('register');
+//
+//Route::post('/register', [RegisteredUserController::class, 'store'])
+//                ->middleware('guest');
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
